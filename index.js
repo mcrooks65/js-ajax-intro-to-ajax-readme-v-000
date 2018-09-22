8f694599a491730fe75363913c389469e8dd52b5
Matthew Crooks <mfcrooks@gmail.com>
2018-09-22 01:19:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 function showRepositories() {
-  //this is set to the XMLHttpRequest object that fired the event
   console.log(this.responseText);
   let repoList = '<ul>';
   for (var i = 0; i < this.responseText.length; i++) {
